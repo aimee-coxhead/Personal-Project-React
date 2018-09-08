2ecda5e5a79961327f757e867d822594403a110a
aimee-coxhead <aimeecoxhead1@gmail.com>
2018-09-08 22:42:45 +1200

    made migrations and seeds

--- migrations/20180908223458_students.js
+++ migrations/20180908223458_students.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex, Promise) {
-  
-};
 
-exports.down = function(knex, Promise) {
+exports.up = (knex, Promise) => {
+    return knex.schema.createTable('students', table => {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('profile_photo')
+      table.integer('timetable_Id')
+    })
+  }
   
-};
+  exports.down = (knex, Promise) => {
+    return knex.schema.dropTable('students')
+  }
--- migrations/20180908223522_timetable.js
+++ migrations/20180908223522_timetable.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex, Promise) {
+exports.up = (knex, Promise) => {
+    return knex.schema.createTable('timetable', table => {
+      table.increments('id').primary()
+      table.string('task_1')
+      table.string('task_2')
+      table.string('task_3')
+      table.string('task_4')
+      table.string('task_5')
+    })
+  }
   
-};
-
-exports.down = function(knex, Promise) {
+  exports.down = (knex, Promise) => {
+    return knex.schema.dropTable('timetable')
+  }
   
-};

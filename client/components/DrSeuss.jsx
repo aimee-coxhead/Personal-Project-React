7e7f1897ee69ff81c60af2ab5046103b8445ff3f
aimee-coxhead <aimeecoxhead1@gmail.com>
2018-09-09 15:26:32 +1200

    adding Dr. Seuss books

--- client/components/DrSeuss.jsx
+++ client/components/DrSeuss.jsx
@@ -6,8 +6,163 @@ export default function DrSeuss (props) {
   return (
     <div>
       <h1>Dr. Seuss Books</h1>
+      <div class="row">
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/rxwGLjQbYzA"><img className="image-book" src="/images/DrSeussBooks/HowTheGrinchStoleChristmas.jpeg" /><h5>How the Grinch Stole Christmas!</h5></a>
+        </div>
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/gfNJP3NR5oI"><img className="image-book" src="/images/DrSeussBooks/TheCatInTheHat.jpg" /><h5>The Cat in the Hat</h5></a>
+        </div>
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/bwkgYs8OhPI"><img className="image-book" src="/images/DrSeussBooks/OhThePlacesYou'llGo.jpg" /><h5>Oh The Places You'll Go!</h5></a>
+        </div>
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/iODY88qZMgo"><img className="image-book" src="/images/DrSeussBooks/TheLorax.jpg" /><h5>The Lorax</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/xTGY5Du77JI"><img className="image-book" src="/images/DrSeussBooks/GreenEggsAndHam.jpg"/><h5>Green Eggs and Ham</h5></a>
+        </div>
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/yDMUu1FkwGM"><img className="image-book" src="/images/DrSeussBooks/FoxInSocks.jpg" /><h5>Fox in Socks</h5></a>
+        </div>
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/wkqJRLpZxgY"><img className="image-book" src="/images/DrSeussBooks/OneFishTwoFishRedFishBlueFish.jpeg" /><h5>One Fish, Two Fish, Red Fish, Blue Fish</h5></a>
+        </div>
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/Ub98eZ_ojOI"><img className="image-book" src="/images/DrSeussBooks/HappyBirthdayToYou.jpg" /><h5>Happy Birthday to You</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/VDfEsPQIOl0"><img className="image-book" src="/images/DrSeussBooks/ABC.jpg" /><h5>ABC</h5></a>
+        </div>
+        <div class="three columns">
+          <a href="https://safeshare.tv/x/uOCORuNuSNo"><img className="image-book" src="/images/DrSeussBooks/There'sAWocketInMyPocket.jpg" /><h5>There's a Wocket in my Pocket</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="/images/DrSeussBooks/HortonHatchesTheEgg.jpg" /><h5>Horten Hatches the Egg</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="/images/DrSeussBooks/TheFootBook.jpg" /><h5>The Foot Book</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="/images/DrSeussBooks/There'sAMapOnMyLap.jpg" /><h5>There's a Map on My Lap</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="/images/DrSeussBooks/ThidwickTheBigHeartedMoose.jpg" /><h5>Thidwick the Big-Harted Moose</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="/images/DrSeussBooks/TenApplesUpOnTop.jpg" /><h5>Ten Apples Up on Top</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Oh The Thinks You can Think</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Sleep Book</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>The Cat in the Hat Comes Back</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Mr. Brown Can Moo, Can You?</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>In a People House</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>The Zax</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Too Many Daves</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Sneetches</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Hop on Pop</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>The Tooth Book</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Sneetches Animated Video</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>What Pet Should I Get?</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Yertle the Turtle Animated Video</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>I Can Read with My Eyes Shut</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Green Eggs and Ham Animated Video</h5></a>
+        </div>
+        <div class="three columns"> 
+           <a href=""><img className="image-book" src="" /><h5>Oh the Places You’ll Go Animated Video</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>If I Ran the Circus</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Hooray for Diffendoofer Day</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Did I Ever Tell You How Lucky You Are?</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>The Butter Battle Book</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>What Was I Scared Of?</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>If I Ran the Zoo</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>You’re Only Old Once</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>A Fish Out of Water</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Marvin K Mooney, Will You Please Go Now?</h5></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>Tadd and Todd</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>The Rabbit, The Bear, and The Zinniga-Zanniga</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>The Big Brag</h5></a>
+        </div>
+        <div class="three columns">
+          <a href=""><img className="image-book" src="" /><h5>On Beyond Zebra</h5></a>
+        </div>
+      </div>
       <div>
-      <Link to="/students">Go Home</Link>
+        <Link to="/students"><button>Go Home</button></Link>
+        <Link to="/students/listen-to-reading"><button>Go Back</button></Link>
       </div>
     </div>
   )
